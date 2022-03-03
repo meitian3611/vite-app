f59ee786590544f12aa4b5aa06e4109f6c8d8e7c
v_tianmei <v_tianmei@tencent.com>
2022-03-03 21:43:44 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,86 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+//@ts-ignore
+import viteCompression from 'vite-plugin-compression'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './', //打包路径
+  plugins: [
+    vue(),
+    // gzip压缩 生产环境生成 .gz 文件
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  // 配置别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@static': path.resolve(__dirname, 'src/assets'),
+    },
+    // 导入时想要省略的扩展名列表  不建议使用 .vue 影响IDE和类型支持
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData: `$injectedColor:orange;`,
+        additionalData: '@import "@/assets/style/mian.scss";',
+      },
+    },
+  },
+  // 引入第三方的配置
+  optimizeDeps: {
+    include: ['axios'],
+  },
+  //启动服务配置
+  server: {
+    cors: true, // 为开发服务器配置 CORS , 默认启用并允许任何源
+    open: true, // 服务启动时自动在浏览器中打开应用
+    https: false, // 是否启用 http 2
+    port: 9000,
+    strictPort: false, //设为true时端口被占用则直接退出，不会尝试下一个可用端口
+    proxy: {
+      '/api': {
+        target: 'http://192.168.99.223:3000', //代理接口
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  // 生产环境打包配置
+  //去除 console debugger
+  build: {
+    target: 'modules',
+    outDir: 'dist', // 指定输出路径
+    emptyOutDir: true, // 默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录。
+    assetsDir: 'static', // 指定生成静态资源的存放路径
+    minify: 'terser', // 混淆器 , terser 构建后文件体积更小
+    terserOptions: {
+      compress: {
+        // 打包生产环境 去除 console 和 debug
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        //静态资源分类打包
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        // manualChunks(id) { //静态资源分拆打包
+        //   if (id.includes('node_modules')) {
+        //     return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        //   }
+        // }
+      },
+    },
+  },
+})
