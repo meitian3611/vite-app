eb2a9860902a7a431e0b661fbfb5bded72808f85
meitian3611 <meitian3611@163.com>
2022-05-08 03:00:02 +0800

    host

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
   plugins: [
     vue(),
     viteMockServe({
-      supportTs: true 
-  }),
+      supportTs: true
+    }),
     // AutoImport({
     //   resolvers: [ElementPlusResolver()],
     // }),
@@ -58,6 +58,7 @@ export default defineConfig({
   },
   //启动服务配置
   server: {
+    host: "0.0.0.0",
     cors: true, // 为开发服务器配置 CORS , 默认启用并允许任何源
     open: true, // 服务启动时自动在浏览器中打开应用
     https: false, // 是否启用 http 2
@@ -79,7 +80,7 @@ export default defineConfig({
     emptyOutDir: true, // 默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录。
     assetsDir: 'static', // 指定生成静态资源的存放路径
     minify: 'terser', // 混淆器 , terser 构建后文件体积更小
-    reportCompressedSize:false, // 启用/禁用 gzip 压缩大小报告。压缩大型输出文件可能会很慢，因此禁用该功能可能会提高大型项目的构建性能。
+    reportCompressedSize: false, // 启用/禁用 gzip 压缩大小报告。压缩大型输出文件可能会很慢，因此禁用该功能可能会提高大型项目的构建性能。
     terserOptions: {
       compress: {
         // 打包生产环境 去除 console 和 debug
